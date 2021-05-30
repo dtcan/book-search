e667efb22c94100dc70c8a64eb6d9fcb40fbdfa6
David Canagasabey <david@dtcan.dev>
2021-05-30 17:01:49 -0400

    Use onError to handle missing image

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,18 +8,12 @@ export default function Book({ book, className }) {
     const hasAuthor = book?.author?.length > 0;
     const hasMultipleAuthors = book?.author?.length > 1;
     
-    // The object tag is used to handle an image not existing, the inner img tag is a fallback
-    // Source: https://stackoverflow.com/questions/980855/
     return <div className={`book ${className}`}>
-        <div className="book-cover">
-            <object data={book?.isbn ? imageSrc(book.isbn) : DEFAULT_COVER} type="image/jpg">
-                <img alt={title} src={DEFAULT_COVER} />
-            </object>
-        </div>
+        <img className="book-cover" alt={title} src={book?.isbn ? imageSrc(book.isbn) : DEFAULT_COVER} onError={e => { e.target.src = DEFAULT_COVER; }} />
         <div className="book-details">
             <h3 className="book-title">{title}</h3>
             <p>
-                <span>Author{hasMultipleAuthors > 0 ? 's' : ''}: {hasAuthor ? book.author.join(', ') : "Unknown"}</span><br />
+                <span>{hasMultipleAuthors > 0 ? 'Authors' : 'Author'}: {hasAuthor ? book.author.join(', ') : "Unknown"}</span><br />
                 <span>First Published: {book?.published || "Unknown"}</span>
             </p>
         </div>
