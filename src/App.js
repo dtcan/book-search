9f6ea3b9476515299e324928575be0850a7aa829
David Canagasabey <david@dtcan.dev>
2021-05-30 17:09:01 -0400

    App layout and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import SearchNav from './components/SearchNav';
+import SearchResult from './components/SearchResult';
 
 function App() {
     return (
         <div className="App">
+            <header>
+                <SearchBar />
+            </header>
+            <main>
+                <SearchNav />
+                <SearchResult />
+                <SearchNav />
+            </main>
         </div>
     );
 }
