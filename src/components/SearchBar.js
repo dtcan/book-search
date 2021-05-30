f93ca378e34060c6d96356377e0e1809cd6b4692
David Canagasabey <david@dtcan.dev>
2021-05-30 19:54:18 -0400

    Type: for prop -> htmlFor prop

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ export default function SearchBar() {
 
     return <div>
         <form className="search" role="search" onSubmit={submit}>
-            <label for="search-input" className="search-label">Book Title</label>
+            <label htmlFor="search-input" className="search-label">Book Title</label>
             <input id="search-input" className="search-input" disabled={loading} type="search" placeholder="Enter book title" />
             <button className="search-button" disabled={loading} type="submit">Search</button>
         </form>
